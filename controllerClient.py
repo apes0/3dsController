4d1ff29e9b73c2e7400d6e19fc699fa3195d56db
apes0 <borislavkosturski@gmail.com>
2021-09-25 22:59:51 +0300

    add better ping handling! :)

--- controllerClient.py
+++ controllerClient.py
@@ -12,16 +12,16 @@
 #
 # all the config variables are in clientConfig.json
 
-import _ctru
-import socket
-import os
 import json
+import os
+import socket
 import struct
 import time
 
+import _ctru
 
 
-def sleep(seconds): # the defualt sleep function from the time module doesnt work, so we need to use this
+def sleep(seconds):  # the defualt sleep function from the time module doesnt work, so we need to use this
     start = time.monotonic()
     while(time.monotonic() < start + seconds):
         pass
@@ -31,67 +31,80 @@ def sleep(seconds): # the defualt sleep function from the time module doesnt wor
 script_dir = os.path.dirname(__file__)  # get script directory
 
 rel_path = 'clientConfig.json'
-abs_file_path = os.path.join(script_dir, rel_path) # open the config file for the client
+# open the config file for the client
+abs_file_path = os.path.join(script_dir, rel_path)
 with open(abs_file_path) as f:
     config = json.loads(f.read())
 
 
-
 def connect():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create a socket
 
-    port = config['port'] # get the port of the server
-    host = config['host'] # get the ip adress of the server
+    port = config['port']  # get the port of the server
+    host = config['host']  # get the ip adress of the server
 
     print(f'connecting to {host} at {port}...')
     try:
-        s.connect((host, port,)) # connect to the server
+        sock.connect((host, port,))  # connect to the server
+        sock.setblocking(False)  # set the socket to non blocking
     except:
         return False
-    
-    return s
 
-s = connect()
+    return sock
+
+
+sock = connect()
+if not sock:
+    raise BaseException("can not connect...")
+
 
 def reconnect():
-    global s # get the socket variable
-    delay = config['reconnect'] # get the amount of reconnects before stopping
-    while delay: # try to reconnect
-        s = connect()
+    global sock  # get the socket variable
+    delay = config['reconnect']  # get the amount of reconnects before stopping
+    while delay:  # try to reconnect
+        sock = connect()
         print('reconnecting...', flush=True)
-        if s: # if a connection is made, return
+        if sock:  # if a connection is made, return
             print('connected!')
             return
-        sleep(5) # wait for 5 seconds untill reconnecting
+        sleep(5)  # wait for 5 seconds untill reconnecting
         delay -= 1
-    raise "can not reconnect..."
+    raise BaseException("can not reconnect...")
 
 
-
-def update(key_mask):
-    packed = struct.pack('!I', key_mask) # pack the current key mask
-
-    print(f'sending buttons ({key_mask})...', flush=True)
+def send(packed):
     try:
-        s.sendall(packed) # send request
+        sock.sendall(packed)  # send request
     except:
-        s.close()
+        sock.close()
         reconnect()
 
 
+def update(key_mask):
+    packed = b'\x01' + struct.pack('!I', key_mask)  # pack the current key mask
 
-wait_frames = config['delay'] # frames to wait between checking inputs
+    print(f'sending buttons ({key_mask})', flush=True)
 
-old_mask = 0 # old key mask
-frames = wait_frames # frame counter for the time from the last input
+    send(packed)
 
+
+wait_frames = config['delay']  # frames to wait between checking inputs
+
+old_mask = 0  # old key mask
+frames = wait_frames  # frame counter for the time from the last input
 
 
 while True:
     _ctru.hid_scan_input()
-    key_mask = _ctru.hid_keys_held() # get held keys
+    key_mask = _ctru.hid_keys_held()  # get held keys
     if old_mask != key_mask and frames > wait_frames:
-        update(key_mask) # tell the server about the change
-        frames = 0 # set the old frame counter to 0
-    old_mask = key_mask # set the old keymask to the current one
-    frames += 1 # increase the frame counter
+        update(key_mask)  # tell the server about the change
+        frames = 0  # set the old frame counter to 0
+    old_mask = key_mask  # set the old keymask to the current one
+    frames += 1  # increase the frame counter
+    try:
+        pinged = sock.recv(1)
+        print('pinged', flush=True)
+        send(b'\x00')  # send a ping to the server
+    except:
+        pass
--- server.py
+++ server.py
@@ -27,78 +27,102 @@
 # button mappings are stored in the folder "configs"
 
 
-import os
 import json
-import keyboard
+import os
+import signal
 import socket
 import struct
-import signal
 import sys
+import time
 
-
+import keyboard
 
 script_dir = os.path.dirname(__file__)  # get script directory
 
 rel_path = 'serverConfig.json'
-abs_file_path = os.path.join(script_dir, rel_path) # open the config file for the server
+# open the config file for the server
+abs_file_path = os.path.join(script_dir, rel_path)
 with open(abs_file_path) as f:
     config = json.loads(f.read())
 
 
-
-options = os.listdir(os.path.join(script_dir, 'configs')) # find all config files
+options = os.listdir(os.path.join(script_dir, 'configs')
+                     )  # find all config files
 
 print('available configs:')
-for i in range(len(options)): # print all options
+for i in range(len(options)):  # print all options
     opt = options[i]
     print(f'{i}. {opt[:-5]}')
 
 opt = input('choose a config to use: ')
 
 if not opt:
-    opt = config['default'] # get the defualt config if none is specified
+    opt = config['default']  # get the defualt config if none is specified
 
 opt = int(opt)
 
-rel_path = options[opt] # get file name
-abs_file_path = os.path.join(script_dir, 'configs', rel_path) # open the config file
+rel_path = options[opt]  # get file name
+abs_file_path = os.path.join(
+    script_dir, 'configs', rel_path)  # open the config file
 with open(abs_file_path) as f:
     buttons = json.loads(f.read())
 
 
+pressed = []  # all pressed buttons
+values = []  # values of all pressed buttons
 
-pressed = [] # all pressed buttons
-values = [] # values of all pressed buttons
+
+start = 0
+
 
 def parse(inp):
     for value, button in buttons.items():
-        value = int(value) # get button value
-        if value & int(inp): # check if the bites are the same
-            if value not in values: # check if the button is already pressed
-                keyboard.press(button) # press the button
+        value = int(value)  # get button value
+        if value & int(inp):  # check if the bites are the same
+            if value not in values:  # check if the button is already pressed
+                keyboard.press(button)  # press the button
                 print(f'pressing {button} ({value})')
-                pressed.append(button) # add the button to the list with pressed buttons
-                values.append(value) # add the button to the list with the values of all buttons
-        elif value in values: # check if the button was pressed, but isn't anyomre
-            pressed.remove(button) # remove the button from the list wit hpressed buttons
-            if button not in pressed: # check if the button should not remain pressed
-                keyboard.release(button) # release the button
+                # add the button to the list with pressed buttons
+                pressed.append(button)
+                # add the button to the list with the values of all buttons
+                values.append(value)
+        elif value in values:  # check if the button was pressed, but isn't anyomre
+            # remove the button from the list wit hpressed buttons
+            pressed.remove(button)
+            if button not in pressed:  # check if the button should not remain pressed
+                keyboard.release(button)  # release the button
                 print(f'releasing {button} ({value})')
-            values.remove(value) # remove the button from the list with all values
+            # remove the button from the list with all values
+            values.remove(value)
 #        print(pressed, value, button)
-
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     def signal_handler(_, __):
-        s.close() # close the socket
-        sys.exit(0) # exit from the program
-    signal.signal(signal.SIGINT, signal_handler) # set a handler for SIGINT
-    s.bind((config['host'], config['port'])) # bind to the adress
+        s.close()  # close the socket
+        sys.exit(0)  # exit from the program
+    signal.signal(signal.SIGINT, signal_handler)  # set a handler for SIGINT
+    s.bind((config['host'], config['port']))  # bind to the adress
+    lastPing = 0
+    ping = 0  # seconds
+    avrPing = 0  # seconds
+    pingMeasurements = 0
     while True:
-        s.listen() # listen for input
-        conn, addr = s.accept() # accept the input
+        s.listen()  # listen for input
+        conn, addr = s.accept()  # accept the input
         while True:
-            data = conn.recv(4) # get the data
-            out = struct.unpack('!I', data)[0] # unpact the data and get the int inside of it
-            parse(out) # parse the input
+            data = conn.recv(1)  # get the data
+            if data == b'\x00':  # ping
+                ping = time.time() - lastPing
+                pingMeasurements += 1
+                avrPing = (avrPing * (pingMeasurements - 1) +
+                           ping) / pingMeasurements
+                print(f'ping is {ping} with an average of {avrPing}')
+            elif data == b'\x01':  # input
+                data = conn.recv(4)
+                # unpact the data and get the int inside of it
+                out = struct.unpack('!I', data)[0]
+                conn.send(b'\x01')  # send ping
+                lastPing = time.time()
+                time.sleep(avrPing/2)
+                parse(out)  # parse the input
